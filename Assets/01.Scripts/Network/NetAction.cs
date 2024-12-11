1019d471707bed09ba463cc6de171f6198c1b701
KoDaMin <98873820+damin06@users.noreply.github.com>
2024-12-11 20:05:37 +0900

    DLL 불리

--- Assets/01.Scripts/Network/NetAction.cs
+++ Assets/01.Scripts/Network/NetAction.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Unity.Netcode;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
--- Assets/01.Scripts/Utill/Util.cs
+++ Assets/01.Scripts/Utill/Util.cs
@@ -69,7 +69,7 @@ namespace Util
 
 namespace Util.Math
 {
-    internal static class MathUtils
+    public static class MathUtils
     {
         public static T RandomEnum<T>()
         {
@@ -81,7 +81,7 @@ namespace Util.Math
         /// 지정된 범위 내에서 랜덤한 Vector3를 생성합니다.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static Vector3 RandomVector3(float minRange, float maxRange)
+        public static Vector3 RandomVector3(float minRange, float maxRange)
         {
             return new Vector3(
                 Random.Range(minRange, maxRange),
@@ -94,7 +94,7 @@ namespace Util.Math
         /// 지정된 범위 내에서 각 축별로 다른 범위를 가진 랜덤한 Vector3를 생성합니다.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static Vector3 RandomVector3(Vector3 minRange, Vector3 maxRange)
+        public static Vector3 RandomVector3(Vector3 minRange, Vector3 maxRange)
         {
             return new Vector3(
                 Random.Range(minRange.x, maxRange.x),
@@ -107,7 +107,7 @@ namespace Util.Math
         /// 지정된 범위 내에서 2D 평면상의 랜덤한 Vector2를 생성합니다.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static Vector2 RandomVector2(float minRange, float maxRange)
+        public static Vector2 RandomVector2(float minRange, float maxRange)
         {
             return new Vector2(
                 Random.Range(minRange, maxRange),
@@ -121,7 +121,7 @@ namespace Util.Math
         /// <param name="minRange">각 축의 최소 범위</param>
         /// <param name="maxRange">각 축의 최대 범위</param>
         /// <returns>랜덤 Vector2</returns>
-        internal static Vector2 RandomVector2(Vector2 minRange, Vector2 maxRange)
+        public static Vector2 RandomVector2(Vector2 minRange, Vector2 maxRange)
         {
             return new Vector2(
                 Random.Range(minRange.x, maxRange.x),
@@ -133,7 +133,7 @@ namespace Util.Math
         /// 값을 다른 범위로 재매핑합니다.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static float Remap(float value, float from1, float to1, float from2, float to2)
+        public static float Remap(float value, float from1, float to1, float from2, float to2)
         {
             return from2 + (value - from1) * (to2 - from2) / (to1 - from1);
         }
@@ -141,7 +141,7 @@ namespace Util.Math
         /// <summary>
         /// 입력값을 출력 범위로 매핑합니다.
         /// </summary>
-        internal static float CalculateMappedValue(float input, float minInput, float maxInput, float minOutput, float maxOutput)
+        public static float CalculateMappedValue(float input, float minInput, float maxInput, float minOutput, float maxOutput)
         {
             float normalized = (input - minInput) / (maxInput - minInput);
             return Mathf.Lerp(maxOutput, minOutput, normalized);
@@ -151,12 +151,12 @@ namespace Util.Math
 
 namespace Util.Component
 {
-    internal static class ComponentUtils
+    public static class ComponentUtils
     {
         /// <summary>
         /// 컴포넌트 자동 주입을 처리합니다.
         /// </summary>
-        internal static void InjectionComponents(object root)
+        public static void InjectionComponents(object root)
         {
             Type type = root.GetType();
             MonoBehaviour script = root as MonoBehaviour;
@@ -184,7 +184,7 @@ namespace Util.Component
         /// <summary>
         /// GameObject에서 컴포넌트를 가져오거나 없으면 추가합니다.
         /// </summary>
-        internal static T GetOrAddComponent<T>(UnityEngine.GameObject obj) where T : UnityEngine.Component
+        public static T GetOrAddComponent<T>(UnityEngine.GameObject obj) where T : UnityEngine.Component
         {
             return obj.TryGetComponent<T>(out T com) ? com : obj.AddComponent<T>();
         }
@@ -193,12 +193,12 @@ namespace Util.Component
 
 namespace Util.Animation
 {
-    internal static class AnimationUtils
+    public static class AnimationUtils
     {
         /// <summary>
         /// 애니메이터 이벤트를 바인딩합니다.
         /// </summary>
-        internal static void BindAnimatorEvent(Animator animator, string animName, Util.AniState state, Event evt)
+        public static void BindAnimatorEvent(Animator animator, string animName, Util.AniState state, Event evt)
         {
             CoroutineHelper.StartCoroutine(BindAnimatorEventRoutine(animator, animName, state, evt));
         }
@@ -216,12 +216,12 @@ namespace Util.Animation
 
 namespace Util.Transform
 {
-    internal static class TransformUtils
+    public static class TransformUtils
     {
         /// <summary>
         /// Transform의 위치를 보간합니다.
         /// </summary>
-        internal static void LerpPosition(UnityEngine.Transform obj, Vector3 startPos, Vector3 endPos)
+        public static void LerpPosition(UnityEngine.Transform obj, Vector3 startPos, Vector3 endPos)
         {
             CoroutineHelper.StartCoroutine(LerpPositionRoutine(obj, startPos, endPos));
         }
@@ -248,7 +248,7 @@ namespace Util.Transform
         /// <summary>
         /// RectTransform의 위치를 보간합니다.
         /// </summary>
-        internal static void LerpRectPosition(RectTransform obj, Vector3 startPos, Vector3 endPos)
+        public static void LerpRectPosition(RectTransform obj, Vector3 startPos, Vector3 endPos)
         {
             CoroutineHelper.StartCoroutine(LerpRectPositionRoutine(obj, startPos, endPos));
         }
@@ -276,12 +276,12 @@ namespace Util.Transform
 
 namespace Util.GameObject
 {
-    internal static class GameObjectUtils
+    public static class GameObjectUtils
     {
         /// <summary>
         /// 자식 브젝트에서 특정 타입의 컴포넌트들을 찾습니다.
         /// </summary>
-        internal static List<T> FindChilds<T>(UnityEngine.GameObject root) where T : UnityEngine.Object
+        public static List<T> FindChilds<T>(UnityEngine.GameObject root) where T : UnityEngine.Object
         {
             if (root == null) return null;
 
@@ -302,7 +302,7 @@ namespace Util.GameObject
         /// <summary>
         /// 특정 이름을 가진 자식 컴포넌트를 찾습니다.
         /// </summary>
-        internal static UnityEngine.Component FindChild(UnityEngine.GameObject root, Type type, string name)
+        public static UnityEngine.Component FindChild(UnityEngine.GameObject root, Type type, string name)
         {
             if (root == null) return null;
 
@@ -322,7 +322,7 @@ namespace Util.GameObject
         /// <summary>
         /// 모든 씬에서 특정 타입의 오브젝트를 찾습니다.
         /// </summary>
-        internal static List<T> FindAllObjects<T>() where T : UnityEngine.Object
+        public static List<T> FindAllObjects<T>() where T : UnityEngine.Object
         {
             List<T> objects = new List<T>();
             for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
